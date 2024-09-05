737205d2dd245682587901f0207d7546a63c8ce5
Anton <antonwilkens11@gmx.de>
2024-09-05 18:35:03 +0200

    Add files via upload

--- AMA Mod/cogs/mod.py
+++ AMA Mod/cogs/mod.py
@@ -29,6 +29,28 @@ class Moderation(commands.Cog):
     async def ping(self, ctx):
         await ctx.send("Pong!")
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        role = discord.utils.get(member.guild.roles, name="Mitglied")
+        await member.add_roles(role)
+
+    @commands.command(name="role")
+    @commands.has_any_role("|| Moderator", "|| Head-Moderator", "|| Admin")
+    async def role(self, ctx, action: str, member: discord.Member, role: discord.Role):
+        if action == "add":
+            if role in member.roles:
+                await ctx.send(f"Diese Rolle hat {member.mention} bereits!")
+            else:
+                await member.add_roles(role)
+                await ctx.send(f"Die Rolle {role.mention} wurde zu {member.mention} hinzugefügt! \n\n**Teammitglied**:{ctx.author.mention}")
+
+        if action == "remove":
+            if role not in member.roles:
+                await ctx.send(f"Diese Rolle hat {member.mention} nicht!")
+            else:
+                await member.remove_roles(role)
+                await ctx.send(f"Die Rolle {role.mention} wurde vom Benutzer {member.mention} entfernt! \n\n**Teammitglied**:{ctx.author.mention}")
+
     @commands.command()
     @commands.has_any_role("|| Moderator", "|| Head_Moderator", "|| Admin", "|| Verified")
     async def warn(self, ctx, member: discord.Member, *, reason=None):
