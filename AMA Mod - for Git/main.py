df9e1ffb3f7180872300cf878acb0f821a43c9bb
Anton <antonwilkens11@gmx.de>
2026-07-19 13:11:45 +0200

    Kleines Update

--- AMA Mod - for Git/main.py
+++ AMA Mod - for Git/main.py
@@ -2,14 +2,16 @@ import discord
 from discord.ext import commands, tasks
 import os
 import asyncio
-from itertools import cycle
-import datetime
 import json
+import emoji
+import re
 
 intents = discord.Intents.all()  # Alle Intents aktivieren
 allowed_mentions = discord.AllowedMentions.none()
-allowed_channel_ids = [1253032238703181946, 1002302289626804244]
+allowed_mention = discord.AllowedMentions(users=True)
+allowed_channel_ids = [] # Worin Commands ausgeführt werden können
 client = commands.Bot(command_prefix=".", intents=intents, allowed_mentions=allowed_mentions)
+custom_emote_pattern = re.compile(r"<a?:\w+:\d+>")  # Discord Custom Emotes
 
 @client.event
 async def on_ready():
@@ -28,21 +30,13 @@ with open("cogs/json/config.json", "r", encoding="utf-8") as f:
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send(no_permission_message)
-    else:
-        await ctx.send("Ein **Fehler** ist aufgetreten!")
 
 @client.event
 async def on_message(message):
     if message.author.id == client.user.id or message.author.bot:
         return
-
-    if any(link in message.content for link in ['http://', 'https://', 'www.']):
-        role = discord.utils.get(message.guild.roles, name="|| Admin")
-        if role not in message.author.roles:
-            await message.delete()
-            await message.channel.send(f"{message.author.mention}, bitte poste keine Links.")
-
-    if message.channel.id == 1234567890123456789 or message.channel.id == 1234567890123456789: # Channel id durch Vorschläge Channel ersetzen
+    
+    if message.channel.id == 123456789 or message.channel.id == 123456789: # Vorschläge vzw Bugreports channel worin automatisch nach jeder nachricht threads erstellt werden
         green_check = discord.utils.get(message.guild.emojis, name='check')
         red_cross = discord.utils.get(message.guild.emojis, name='cross')
         thread = await message.channel.create_thread(
@@ -52,8 +46,35 @@ async def on_message(message):
         )
         await message.add_reaction(green_check)
         await message.add_reaction(red_cross)
-        await thread.send(f"{message.author.mention} hier kannst du mit anderen über deine Idee diskutieren!")
+        await thread.send(f"{message.author.mention} hier kannst du mit anderen über deine Idee diskutieren!", allowed_mentions=allowed_mention)
 
+    unicode_emojis = [char for char in message.content if char in emoji.EMOJI_DATA]
+    custom_emotes = custom_emote_pattern.findall(message.content)  # Benutzerdefinierte Emotes finden
+    total_emotes = len(unicode_emojis) + len(custom_emotes)        # Gesamtanzahl berechnen
+    
+    if total_emotes >= 6:
+        await message.delete()
+        await message.channel.send(
+            f"{message.author.mention}, **bitte nicht zu viele Emotes senden!**\n_Nur eine Hinweis, keine Verwarnung_",
+            delete_after=10, allowed_mentions=allowed_mention)
+
+    if any(link in message.content for link in ['http://', 'https://', 'www.', 'discord.gg/','discord.com/invite/']):
+        role = discord.utils.get(message.guild.roles, name="|| Admin")
+        linkrole = discord.utils.get(message.guild.roles, id=1305620568615292928)
+        if role not in message.author.roles and linkrole not in message.author.roles:
+            await message.delete()
+            await message.channel.send(f"{message.author.mention}, **bitte keine Links senden!**\n_Nur eine Hinweis, keine Verwarnung_", delete_after=10, allowed_mentions=allowed_mention)
+                    
+        # Muster für verbotene Links
+        forbidden_links = ['discord.gg/', 'discord.com/invite/', 'youtube.com/channel', 'youtube.com/@']
+            
+        # Benutzer mit der `linkrole`: nur Discord-Server- und YouTube-Kanal-Links verbieten
+        if linkrole in message.author.roles and role not in message.author.roles:
+            if any(link in message.content for link in forbidden_links):
+                await message.delete()
+                await message.channel.send(f"{message.author.mention}, **bitte keine Youtubekanal- oder Discordserver-links senden!**\n_Nur eine Hinweis, keine Verwarnung_", delete_after=10, allowed_mentions=allowed_mention)
+                return
+            
     await client.process_commands(message)
 
 @client.command()
@@ -63,7 +84,7 @@ async def rule(ctx):
 
 @tasks.loop(hours=48)
 async def rule():
-    channel = client.get_channel(1002266257103540289)
+    channel = client.get_channel() # Chat Channel worin alle 48 Stunden auf die Regeln hingewiesen wird
     await channel.send(ruletext)
 
 @client.event
@@ -92,35 +113,15 @@ async def on_guild_remove(guild):
 
 @client.command(aliases=["hallo", "moin"])
 async def hello(ctx):
-    await ctx.send(f"Hallo, {ctx.author.mention}!")
-
-@client.command()
-async def stinky(ctx, member: discord.Member=None):
-    # Überprüfen, ob der Befehl in einem der erlaubten Channels verwendet wird
-    if ctx.channel.id not in allowed_channel_ids:
-        return
-
-    if member is None:
-        member = ctx.author
-    await ctx.send(f"{member.mention}, du stinkst!")
+    await ctx.send(f"Hallo, {ctx.author.mention}!", allowed_mentions=allowed_mention)
 
 @client.tree.command(name="credits", description="Dieser command zeigt die Credits vom Bot")
 async def credits(interaction: discord.Interaction): 
-    await interaction.response.send_message("Dieser Bot wird **programmiert** von **@anton1_1661** \nDiscord Server: https://www.discord.gg/a3KGf4rpJ6",)
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def sendembed(ctx):
-    embeded_msg = discord.Embed(title="Titel vom embed", description="Beschreibeung vom embed", color=discord.Color.blue())
-    embeded_msg.set_thumbnail(url=ctx.guild.icon)
-    embeded_msg.add_field(name="Name vom Feld", value="Inhalt vom Feld", inline=False)
-    embeded_msg.set_footer(text="Autor unten", icon_url=ctx.author.avatar)
-    embeded_msg.set_author(name="Autor oben", icon_url=ctx.author.avatar)
-    await ctx.send(embed=embeded_msg)
+    await interaction.response.send_message("Dieser Bot wird **programmiert** von **@anton1_1661** \nDiscord Server: https://www.discord.gg/a3KGf4rpJ6", ephemeral=True)
 
 @client.tree.command(name="help", description="Hier ist noch wenig los.")
 async def help(interaction: discord.Interaction):
-    await interaction.response.send_message(f"<#1253032238703181946>")
+    await interaction.response.send_message(f"<#1283041444558405633>", ephemeral=True)
 
 async def Load():
     for filename in os.listdir("./cogs"):
@@ -130,6 +131,6 @@ async def Load():
 async def main():
     async with client:
         await Load()
-        await client.start(token) # Macht etwas 
+        await client.start(token)
 
 asyncio.run(main())
